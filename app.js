1613969b278099bbd3ef1d36bd74ee5f2633c86b
Alvin Febriando <alvinfebriando@outlook.com>
2019-12-29 12:05:03 +0700

    Create function to fetch necessary data

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const http = rateLimit(axios.default.create(), {
   maxRequests: 60,
   perMilliseconds: 60 * 1000
 });
+
+const getPublicMatches = async () => {
+  let response = await http.get('https://api.opendota.com/api/publicMatches');
+  return response;
+};
+
+const getMatchData = async match_id => {
+  let response = await http.get(
+    `https://api.opendota.com/api/matches/${match_id}`
+  );
+  return response;
+};
