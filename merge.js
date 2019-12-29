549dd40136e5602912e049dcbb36fc1743683c8e
Alvin Febriando <alvinfebriando@outlook.com>
2019-12-29 13:31:38 +0700

    Add merge all output file into matches.json

---
+++ merge.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+
+const merge = async () => {
+  let uniqueId = new Set();
+  let files = await fs.readdir('./output');
+  let merged = [];
+
+  const filesContentPromises = files.map(async file => {
+    let matches = await fs.readFile(`./output/${file}`, 'utf-8');
+    try {
+      matches = JSON.parse(matches);
+      matches.map(match => {
+        if (!uniqueId.has(match.match_id)) {
+          uniqueId.add(match.match_id);
+          merged.push(match);
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  await Promise.all(filesContentPromises);
+  await fs.writeFile('./matches.json', JSON.stringify(merged));
+  merged = await fs.readFile('./matches.json', 'utf-8');
+  console.log(`Total data: ${JSON.parse(merged).length}`);
+};
+
+module.exports = merge;
