48997cc72fe40a02e1a28440cf9e48ab04cc0ba4
Alvin Febriando <alvinfebriando@outlook.com>
2020-01-06 22:58:21 +0700

    Filter each of JSON data and store in filtered/

--- filter.js
+++ filter.js
@@ -1,37 +1,40 @@
 const fs = require('fs').promises;
 
-const getMatches = async () => {
-  let matches = await fs.readFile('./matches.json', 'utf-8');
+const getMatches = async file => {
+  let matches = await fs.readFile(`./output/${file}`, 'utf-8');
   return JSON.parse(matches);
 };
 
 const filter = async () => {
-  let matches = await getMatches();
-  let filteredMatches = [];
+  let files = await fs.readdir('./output');
+  const fileContentPromises = files.map(async file => {
+    let matches = await getMatches(file);
+    let filteredMatches = [];
 
-  for (const match of matches) {
-    filteredMatches.push({
-      match_id: match.match_id,
-      duration: match.duration,
-      radiant_win: match.radiant_win,
-      game_mode: match.game_mode,
-      lobby_type: match.lobby_type,
-      radiant_score: match.radiant_score,
-      dire_score: match.dire_score,
-      start_time: match.start_time,
-      replay_url: match.replay_url,
-      version: match.version,
-      patch: match.patch_id,
-      region: match.region
-    });
-  }
+    for (const match of matches) {
+      filteredMatches.push({
+        match_id: match.match_id,
+        duration: match.duration,
+        radiant_win: match.radiant_win,
+        game_mode: match.game_mode,
+        lobby_type: match.lobby_type,
+        radiant_score: match.radiant_score,
+        dire_score: match.dire_score,
+        start_time: match.start_time,
+        replay_url: match.replay_url,
+        version: match.version,
+        patch: match.patch_id,
+        region: match.region
+      });
+    }
+    await fs.writeFile(
+      `./filtered/${file}`,
+      JSON.stringify(filteredMatches, null, 2)
+    );
+    console.log(`${file} filtered`);
+  });
 
-  await fs.writeFile(
-    './filtered_matches.json',
-    JSON.stringify(filteredMatches, null, 2)
-  );
-
-  console.log(`Filtered ${filteredMatches.length} matches`);
+  await Promise.all(fileContentPromises);
 };
 
 module.exports = filter;
